1f2b76760d1e420bb43098a92531f51a9995f01b
kshit <kshit@kshitiz_gupta>
2024-08-20 13:27:13 +0530

    Parallel Execution

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -16,41 +16,50 @@ import utilites.Utilites;
 
 public class DriverFactory {
 	
+	
+	
 	static WebDriver driver = null;
 	
-	
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 	
 	public static WebDriver initializeBrowser(String browser) {
 		
 		if(browser.equalsIgnoreCase("chrome")) {
 			ChromeOptions option = new ChromeOptions();
 			option.addArguments("--incognito");
-			driver = new ChromeDriver(option);
+			tlDriver.set(new ChromeDriver(option));
+	
 		}else if(browser.equalsIgnoreCase("firefox")) {
 			FirefoxOptions option = new FirefoxOptions();
 			option.addArguments("--incognito");
-			driver = new FirefoxDriver(option);
+			tlDriver.set(new FirefoxDriver(option));
+		
 		}else if(browser.equalsIgnoreCase("edge")) {
 			EdgeOptions option = new EdgeOptions();
 			option.addArguments("--incognito");
-			driver = new EdgeDriver(option);
+			tlDriver.set(new EdgeDriver(option));
+		
 		}else if(browser.equalsIgnoreCase("safari")) {
-			driver = new SafariDriver();
+			tlDriver.set(new SafariDriver());
+	
 		}
 		
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIMEOUT));
-		Utilites utilites = new Utilites();
-		driver.get(utilites.fileReader("url"));
 		
-		return driver;
+		driverWebDriver().manage().deleteAllCookies();
+		driverWebDriver().manage().window().maximize();
+		driverWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
+		driverWebDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIMEOUT));
+		Utilites utilites = new Utilites();
+		driverWebDriver().get(utilites.fileReader("url"));
+		
+		return driverWebDriver();
 	}
 	
 	
-	public static WebDriver driverWebDriver() {
-		return driver;
+	public static synchronized WebDriver driverWebDriver() {
+		
+		return tlDriver.get();
+	
 	}
 
 }
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,3 +17,5 @@ import io.cucumber.junit.CucumberOptions;
 public class TestRunner {
 
 }
+
+
